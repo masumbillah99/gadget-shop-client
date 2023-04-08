8732a39ed74c47fa138f17a221d0d936dde999e9
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-04-08 14:10:11 +0600

    explore conditiona rendering & conditonal css class

--- bangla-tshirt-project/src/components/Cart/Cart.jsx
+++ bangla-tshirt-project/src/components/Cart/Cart.jsx
@@ -2,23 +2,52 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+  let message;
+  if (cart.length === 0) {
+    message = <p className="warn-msg">Please add some products</p>;
+  }
+  // else {
+  //   message = <p>Boro-loxxx Rustom</p>;
+  // }
+
   return (
     <div className="cart-details">
       <h2>Order Summary: {cart.length}</h2>
       <hr />
+
+      {/* {cart.length ? (
+        <span className="plum">Rich Rustom</span>
+      ) : (
+        <span className="red">Fokira Rustom</span>
+      )} */}
+
+      {message}
+
       {cart.map((tshirt) => (
         <div key={tshirt._id}>
           {tshirt.name}
           <button onClick={() => handleRemoveFromCart(tshirt._id)}>X</button>
         </div>
       ))}
+
+      {/* {cart.length === 2 && <p>Double Bonanza!</p>}
+      {cart.length === 3 || <h3>Not same is 3 product</h3>} */}
     </div>
   );
 };
 
 export default Cart;
 
-
 /** 
  * Conditional Rendering
+    1. use if/else to set a variable that will contain an element, components
+    2. ternary operator: condition ? 'for true' : 'false'
+    3. Logical && : (if the condition is true then the next thing will be displayed)
+    4. Logical || : (if the condition is false then the next thing will be displayed)
+*/
+
+/**
+ * Conditional CSS class
+    1. use ternary 
+    2. ternary inside template string
 */
